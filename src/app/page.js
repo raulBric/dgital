c24890e45bc209c6154da51e0e53e99ed5563af8
Raul <raul.bricio@gmail.com>
2024-08-22 22:47:23 +0200

    header

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header/>
-    </main>
+    <header className="bg-white">
+      <div className="bg-black h-10 w-full flex items-center justify-end lg:justify-end">
+        <span className="text-white mr-4 cursor-pointer">Es</span>
+        <span className="text-white mr-8 cursor-pointer">Cat</span>
+      </div>
+      <div className="flex justify-center">
+        <Header />
+      </div>
+    </header>
   );
 }
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
+import Logo from '../assets/dgital_logo.webp';
 
 export default function Header() {
     return (
-        <header>
-        <h1>Hola Como estas</h1>
+        <header className="flex flex-col items-center">
+            <Image 
+                src={Logo}
+                alt="logo" 
+                width={200} 
+                height={200} 
+                className='m-4'
+            />
+            <div className="mt-4">
+                <button className="btn btn-primary">Primary</button>
+            </div>
         </header>
     );
-    }
+}
