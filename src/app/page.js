3dc5870a8ec1a982f49e8a3202df08db7fb1488f
Raul <raul.bricio@gmail.com>
2024-09-17 22:09:44 +0200

    1709

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
 import SeccionPrincipal from "@/components/SeccionPrincipal";
 import CustomCard from "@/components/CustomCard"; // Importación correcta de CustomCard
+import QuienSomos from "@/components/QuienSomos";
 
 export default function Home() {
   return (
     <header className="bg-white">
       <Header />
       <SeccionPrincipal />
+      <QuienSomos />
     </header>
   );
 }
---
+++ src/components/QuienSomos.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+export default function QuienSomos() {
+  return (
+    <div className="bg-blue-200 min-h-80 pt-10 rounded-tr-[100px]">
+      <h1 className=" bg-gradient-to-l from-blue-400 to-blue-900 bg-clip-text text-transparent font-bold text-4xl text-center pt-10">Valores</h1> 
+    </div>
+  )
+}
+
+ 
