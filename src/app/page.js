16e414ad1a6a468bc728afc2dd4756b4e73e2693
Raul <raul.bricio@gmail.com>
2024-10-29 18:13:52 +0100

    Refactor HeroSection component to include DarkModeProvider

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 
 import Container from "@/components/Container";
+import { DarkModeProvider } from "@/context/Mycontext";
 
 
 export default function Home() {
   return (
-    <header className="bg-white">
-      <Container />
-    </header>
+    <DarkModeProvider>
+      <header className="bg-white">
+        <Container />
+      </header>
+    </DarkModeProvider>
+
   );
 }
 
