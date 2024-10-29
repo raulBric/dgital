c7e50d94e1eb708c3d3a70218f243fa27d87163b
Raul <raul.bricio@gmail.com>
2024-10-29 20:24:07 +0100

    Refactor FAQSection component to remove dark mode support

--- src/components/FAQSection.jsx
+++ src/components/FAQSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FAQSection = () => {
   return (
-    <div className="container mx-auto px-4 py-8 dark:bg-slate-700">
+    <div className="container mx-auto px-4 py-8">
       <h2 className="text-4xl font-extrabold text-gray-800 dark:text-gray-100 text-center m-12">FAQS</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-12">
         {/* Columna 1 */}
