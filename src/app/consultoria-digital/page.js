0645ebaa9bcba2a58c07f821eb7e6a893f75ac36
Raul <raul.bricio@gmail.com>
2024-12-23 12:03:45 +0100

    Add WhatsAppButton component to ServiciosPage

--- src/app/consultoria-digital/page.js
+++ src/app/consultoria-digital/page.js
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
 import DigitalConsultingService from "@/components/servicios/DigitalConsultingService";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsappButton";
 
 export default function ServiciosPage() {
   return (
     <header className="bg-gradient-to-br from-gray-900 to-gray-800">
       <Header />  
       <DigitalConsultingService />
+      <WhatsAppButton />
       <Footer />
     </header>
   );
